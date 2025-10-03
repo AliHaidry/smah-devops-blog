fdbc9bcedb617cab8f7023585ddec1d957e862d7
Syed Muhammad Ali Haidry <alihaidry11@gmail.com>
2025-10-03 22:24:35 +0500

    Code Updated - content updated!!!

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Syed Muhammad Ali Haidry',
   headerTitle: 'DevOps Blog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A blog about DevOps, Cloud Computing, and Software Development.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
